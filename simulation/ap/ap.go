4097c55c38d9ce8946b93810d3eeab8cbad0b203
sacca97 <tommaso.sacchetti@gmail.com>
2021-04-30 14:07:02 +0200

    adding beta branch ffs

--- simulation/ap/ap.go
+++ simulation/ap/ap.go
@@ -1,31 +1,37 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/hmac"
 	"crypto/rand"
 	"crypto/sha1"
 	"encoding/binary"
-	"crypto/aes"
-	"crypto/cipher"
+	"errors"
 	"fmt"
 	"log"
 	"net"
-	"golang.org/x/crypto/pbkdf2"
+
 	"github.com/google/go-cmp/cmp"
+	"golang.org/x/crypto/pbkdf2"
 )
 
+//ap is listening on port 8000
+//mitm on port 8001 from ap and 8002 from client to simplify
+//client is port 8003
 var ap net.Listener
 
 type handshakeState struct {
-	msgcount int
+	msgcount     int
 	ptkinstalled bool
-	pmk      []byte
-	ptk      []byte
-	Anonce   []byte
-	Snonce   []byte
-	replay   uint64
+	pmk          []byte
+	ptk          []byte
+	Anonce       []byte
+	Snonce       []byte
+	replay       uint64
 }
 
+//I don't think I'll implement a packet structure but just let this here
 type packet struct {
 	descr         uint8
 	info          uint16
@@ -37,37 +43,42 @@ type packet struct {
 
 var hs handshakeState
 
-func send(msg []byte) {
-	// connect to mitm
-	conn, err := net.Dial("tcp", "127.0.0.1:8003")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer conn.Close()
-	conn.Write(msg)
+const (
+	password = "abcdefgh"
+	ssid     = "wpa2simulation"
+)
 
-}
-
-func recv() {
+func recv(s net.Conn) {
+	msg := make([]byte, 256)
 	for {
-		s, _ := ap.Accept()
-		msg := make([]byte, 512)
-		n, _ := s.Read(msg)
-		msg = msg[:n]
+		n, err := s.Read(msg)
+		if err != nil {
+			log.Println("Connection closed by the client")
+			return
+		}
 		switch msg[0] {
 		case 2:
-			handleMsg2(msg)
-			sendMsg3()
-			//hs.msgcount++
+			handleMsg2(msg[:n])
+			hs.msgcount++
+			m := buildMsg3()
+			send(m, s)
+			log.Println("Sent msg 3/4")
 		case 4:
-			handleMsg4(msg)
-		default:
-			pt, _ := decrypt(msg)
+			handleMsg4(msg[:n])
+			hs.msgcount++
+		case 5: //encrypted data packet
+			pt, err := handleEncrypted(msg[:n])
+			if err != nil {
+				log.Println(err)
+			}
 			log.Println("Data: ", string(pt))
-
 		}
 	}
+}
+
+func send(msg []byte, conn net.Conn) {
+	conn.Write(msg)
+	hs.replay++
 }
 
 func nonce() []byte {
@@ -88,63 +99,96 @@ func hmac_hash(msg, key []byte) []byte {
 func init() {
 	//intializing values
 	hs = handshakeState{
-		msgcount: 0,
+		msgcount:     0,
 		ptkinstalled: false,
-		pmk:      pbkdf2.Key([]byte("arandompassword"), []byte("testnetwork"), 4096, 32, sha1.New),
-		ptk:      []byte{},
-		Anonce:   nonce(),
-		Snonce:   []byte{},
-		replay:   1,
+		pmk:          pbkdf2.Key([]byte(password), []byte(ssid), 4096, 32, sha1.New),
+		ptk:          []byte{},
+		Anonce:       nonce(),
+		Snonce:       make([]byte, 32),
+		replay:       0,
 	}
 }
 
 func handleMsg2(tmp []byte) {
-	log.Println("Received handshake message 2/4")
-	hs.Snonce = append([]byte{}, tmp[1:33]...) //probably to cut the size
-	t := append(hs.Anonce, hs.Snonce...)
-	t1 := append([]byte("BD:B2:12:3F:18:F9"), []byte("6D:BF:EC:03:F0:2B")...)
-	t2 := append(t, t1...)
-	hs.ptk = pbkdf2.Key(hs.pmk, t2, 4096, 64, sha1.New) //actually md5
-	//tk is hs.ptk[64:192]
-	if !verifyMIC(tmp[:33],tmp[33:]){
+	//tmp[0] is message type
+	//tmp[1:9] is the counter
+	copy(hs.Snonce, tmp[9:41])
+	rnd := make([]byte, 98)
+	copy(rnd[:32], hs.Anonce)
+	copy(rnd[32:64], hs.Snonce)
+	copy(rnd[64:81], []byte("BD:B2:12:3F:18:F9"))
+	copy(rnd[81:], []byte("6D:BF:EC:03:F0:2B"))
+
+	hs.ptk = pbkdf2.Key(hs.pmk, rnd, 4096, 64, sha1.New)
+	//mic := hmac_hash(tmp[:41], hs.ptk[:16])[:16]
+
+	if !verifyMIC(tmp[:len(tmp)-16], tmp[len(tmp)-16:]) {
 		log.Fatal("MIC check failed")
 	}
 	log.Println("MIC check valid")
 	//client is auth here...
 }
 
-func encrypt(msg []byte) []byte{
+func encrypt(msg []byte) []byte {
+	//Ecnryption key in WPA2 is the TK, taken from those bits of the PTK
 	key := append([]byte{}, hs.ptk[32:48]...)
 	nonce := make([]byte, 12)
 	binary.LittleEndian.PutUint64(nonce, hs.replay)
 	b, _ := aes.NewCipher(key)
 	c, _ := cipher.NewGCM(b)
-	hs.replay++
 	return c.Seal(nil, nonce, msg, nil)
+}
+
+func handleEncrypted(msg []byte) ([]byte, error) {
+	remoteCounter := binary.LittleEndian.Uint64(msg[1:9])
+	log.Println(msg)
+	if remoteCounter == hs.replay+1 {
+		hs.replay++
+		return decrypt(msg[9:])
+	}
+	return []byte{}, errors.New("Replay counter mismatch")
 }
 
 func decrypt(msg []byte) ([]byte, error) {
 	key := append([]byte{}, hs.ptk[32:48]...)
 	nonce := make([]byte, 12)
 	binary.LittleEndian.PutUint64(nonce, hs.replay)
+	log.Println(hs.replay, msg)
 	b, _ := aes.NewCipher(key)
 	c, _ := cipher.NewGCM(b)
-	hs.replay++
 	return c.Open(nil, nonce, msg, nil)
 }
 
+func buildMsg1() []byte {
+	msg := make([]byte, 41)
+	msg[0] = 1
+	binary.BigEndian.PutUint64(msg[1:9], hs.replay)
+	copy(msg[9:], hs.Anonce)
+	return msg
+}
 
-func sendMsg3() {
+func buildMsg3() []byte {
+	msg := make([]byte, 36)
+	msg[0] = 3
+	binary.BigEndian.PutUint64(msg[1:9], hs.replay)
+	copy(msg[9:20], []byte("install_key"))
+	//msg := append([]byte(hs.replay), []byte("install key")...)
+	mic := hmac_hash(msg[:20], hs.ptk[:16])[:16]
+	copy(msg[20:], mic)
+	return msg
+}
+
+func sendMsg3(s net.Conn) {
 	msg := append([]byte{3}, []byte("key_installation")...)
 	mic := hmac_hash(msg, hs.ptk[:16])[:16]
-	send(append(msg, mic...))//msg 3 of 4 must be stopped from attacker
+	s.Write(append(msg, mic...))
+	//send(append(msg, mic...)) //msg 3 of 4 must be stopped from attacker
 	log.Println("Sent handshake message 3/4")
 }
 
 func handleMsg4(tmp []byte) {
-	log.Println("Received handshake message 4/4")
 
-	if !verifyMIC(tmp[:len(tmp)-16],tmp[len(tmp)-16:]) {
+	if !verifyMIC(tmp[:len(tmp)-16], tmp[len(tmp)-16:]) {
 		log.Fatal("MIC check failed")
 	}
 	//installing current PTK
@@ -154,9 +198,9 @@ func handleMsg4(tmp []byte) {
 	log.Println("PTK installed")
 }
 
-func verifyMIC(data, recvMic []byte) bool{
+func verifyMIC(data, recvMic []byte) bool {
 	mic := hmac_hash(data, hs.ptk[:16])[:16]
-	return cmp.Equal(mic,recvMic)
+	return cmp.Equal(mic, recvMic)
 }
 
 func main() {
@@ -169,10 +213,16 @@ func main() {
 		return
 	}
 
-	go recv()
-	msg1 := append([]byte{1}, hs.Anonce...)
-	send(msg1)//msg 1 of 4
-	fmt.Println("Sent handshake message 1/4")
+	fmt.Println("Waiting for conn..")
+	s, _ := ap.Accept()
+	fmt.Println("Connected")
+
+	go recv(s)
+
+	m := buildMsg1()
+	hs.msgcount++
+	send(m, s)
+	log.Println("Sent msg 1/4")
 	for {
 		if hs.msgcount == 5 {
 			break
--- simulation/client/client.go
+++ simulation/client/client.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"crypto/aes"
-	"github.com/google/go-cmp/cmp"
+	"crypto/cipher"
 	"crypto/hmac"
 	"crypto/rand"
 	"crypto/sha1"
@@ -11,10 +11,13 @@ import (
 	"log"
 	"net"
 	"time"
-	"crypto/cipher"
+
+	"github.com/google/go-cmp/cmp"
 	"golang.org/x/crypto/pbkdf2"
 )
 
+//a lot of functions can be abstracted into a separated module
+//but for now let's just keep them this way
 var client net.Listener
 
 type handshakeState struct {
@@ -29,33 +32,43 @@ type handshakeState struct {
 
 var hs handshakeState
 
-func send(msg []byte) {
-	// connect to mitm
-	conn, err := net.Dial("tcp", "127.0.0.1:8000")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer conn.Close()
-	conn.Write(msg)
+const (
+	password = "abcdefgh"
+	ssid     = "wpa2simulation"
+)
 
+func send(msg []byte, conn net.Conn) {
+	conn.Write(msg)
+	hs.replay++
 }
 
-func recv() {
+func recv(s net.Conn) {
+
+	//defer conn.Close()
+	msg := make([]byte, 256)
 	for {
-		s, _ := client.Accept()
-		msg := make([]byte, 512)
-		n, _ := s.Read(msg)
-		msg = msg[:n]
-		//tmp contains the actual message
+		n, err := s.Read(msg)
+
+		if err != nil {
+			log.Println("Connection closed by the server")
+			return
+		}
+		if n == 0 {
+			continue
+		}
 		switch msg[0] {
 		case 1:
 			initialize()
-			handleMsg1(msg)
-			sendMsg2()
+			handleMsg1(msg[:n])
+			m := buildMsg2()
+			send(m, s)
+			log.Println("Sent msg 2/4")
 		case 3:
-			handleMsg3(msg)
-			sendMsg4()
+			handleMsg3(msg[:n])
+			m := buildMsg4()
+			send(m, s)
+			log.Println("Sent msg 4/4")
+			go sendDataPacket(s)
 		default:
 			fmt.Println("Wait...")
 		}
@@ -77,90 +90,118 @@ func hmac_hash(msg, key []byte) []byte {
 	return h.Sum(nil)
 }
 
-func initialize(){
+func initialize() {
 	hs = handshakeState{
 		msgcount:     0,
 		ptkinstalled: false,
-		pmk:          pbkdf2.Key([]byte("arandompassword"), []byte("testnetwork"), 4096, 32, sha1.New),
+		pmk:          pbkdf2.Key([]byte(password), []byte(ssid), 4096, 32, sha1.New),
 		ptk:          []byte{},
-		Anonce:       []byte{},
+		Anonce:       make([]byte, 32),
 		Snonce:       nonce(),
-		replay:       1,
+		replay:       0,
 	}
 }
 
 func handleMsg1(tmp []byte) {
-	log.Println("Received handshake message 1/4")
-	hs.Anonce = append([]byte{}, tmp[1:33]...)
-	t := append(hs.Anonce, hs.Snonce...)
-	t1 := append([]byte("BD:B2:12:3F:18:F9"), []byte("6D:BF:EC:03:F0:2B")...)
-	t2 := append(t, t1...)
-	hs.ptk = pbkdf2.Key(hs.pmk, t2, 4096, 64, sha1.New)
+	//tmp[0] is message type
+	//tmp[1:9] is the counter
+	copy(hs.Anonce, tmp[9:41])
+	rnd := make([]byte, 98)
+	copy(rnd[:32], hs.Anonce)
+	copy(rnd[32:64], hs.Snonce)
+	copy(rnd[64:81], []byte("BD:B2:12:3F:18:F9"))
+	copy(rnd[81:], []byte("6D:BF:EC:03:F0:2B"))
+
+	hs.ptk = pbkdf2.Key(hs.pmk, rnd, 4096, 64, sha1.New)
 }
 
-func sendMsg2(){
-	log.Println("Sent handshake message 2/4")
-	msg := append([]byte{2}, hs.Snonce...)
-	mic := hmac_hash(msg, hs.ptk[:16])[:16]
-	send(append(msg, mic...))//msg 2 of 4
+func buildMsg2() []byte {
+	msg := make([]byte, 57)
+	msg[0] = 2
+	binary.BigEndian.PutUint64(msg[1:9], hs.replay)
+	copy(msg[9:41], hs.Snonce)
+	//msg := append([]byte{2}, hs.Snonce...)
+	mic := hmac_hash(msg[:41], hs.ptk[:16])[:16]
+	copy(msg[41:], mic)
+
+	return msg
+	//s.Write(append(msg, mic...)) //msg 2 of 4
 }
 
 func handleMsg3(tmp []byte) {
-	log.Println("Received handshake message 3/4")
-	if !verifyMIC(tmp[:len(tmp)-16],tmp[len(tmp)-16:]) {
+	log.Println("Received msg 3/4")
+	if !verifyMIC(tmp[:len(tmp)-16], tmp[len(tmp)-16:]) {
 		log.Fatal("MIC check failed")
 	}
 	log.Println("MIC check valid")
 }
 
-func sendMsg4() {
+func buildMsg4() []byte {
 	//installing current PTK
-	hs.replay = 0
+	msg := make([]byte, 44)
+	msg[0] = 4
+	binary.BigEndian.PutUint64(msg[1:9], hs.replay)
+	copy(msg[9:28], []byte("install_key_confirm"))
+	mic := hmac_hash(msg[:28], hs.ptk[:16])[:16]
+	copy(msg[28:], mic)
 	hs.ptkinstalled = true
 	log.Println("PTK installed")
-	msg := append([]byte{4}, []byte("key_installation_confirm")...)
-	mic := hmac_hash(msg, hs.ptk[:16])[:16]
-	send(append(msg, mic...))
-	log.Println("Sent handshake message 4/4")
-	go sendData()
+
+	hs.replay = 0
+	return msg
+
+	//msg := append([]byte{4}, []byte("key_installation_confirm")...)
+	//mic := hmac_hash(msg, hs.ptk[:16])[:16]
+	//s.Write(append(msg, mic...))
+	//log.Println("Sent handshake message 4/4")
+	//go sendData()
 }
 
-func sendData(){
-	time.Sleep(5*time.Second)
-	send(encrypt([]byte("Simolachecazzoesimola")))
+func sendDataPacket(s net.Conn) {
+	time.Sleep(5 * time.Second)
+	m := []byte("thisisatestmessage")
+	msg := make([]byte, 9)
+	msg[0] = 5
+	binary.LittleEndian.PutUint64(msg[1:9], hs.replay)
+	enc := encrypt(m)
+	log.Println("Sending encrypted packet...")
+
+	send(append(msg, enc...), s)
 }
 
-func installPTK(){
+func installPTK() {
 	//on msg3 receive verify the MIC and install the PTK
-
 
 	//send msg 4 of 4 telling AP to install the PTK with a MIC
 }
 
-func verifyMIC(data, recvMic []byte) bool{
+func verifyMIC(data, recvMic []byte) bool {
 	mic := hmac_hash(data, hs.ptk[:16])[:16]
-	return cmp.Equal(mic,recvMic)
+	return cmp.Equal(mic, recvMic)
 }
 
-func encrypt(msg []byte) []byte{
+func encrypt(msg []byte) []byte {
 	key := append([]byte{}, hs.ptk[32:48]...)
-	nonce := make([]byte, 12)
-	binary.LittleEndian.PutUint64(nonce, hs.replay)
+	n := make([]byte, 12)
+	binary.LittleEndian.PutUint64(n, hs.replay)
+	log.Println(hs.replay)
 	b, _ := aes.NewCipher(key)
 	c, _ := cipher.NewGCM(b)
-	hs.replay++
-	ct := c.Seal(nil, nonce, msg, nil)
-	log.Println("Data: ", ct)
+	ct := c.Seal(nil, n, msg, nil)
 	return ct
 }
 
-
 func main() {
-	var err error
-	client, err = net.Listen("tcp", ":8003")
+	//var err error
+	//client, err = net.Listen("tcp", ":8003")
+	//if err != nil {
+	//	log.Fatal(err)
+	//	return
+	//}
+	s, err := net.Dial("tcp", "127.0.0.1:8000")
+
 	if err != nil {
-		log.Fatal(err)
-		return
+		log.Fatal("Server unavailable")
 	}
-	recv()
+	recv(s)
 }
