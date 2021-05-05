18878fbbbe679f1b3af29108bd299d792752f178
sacca97 <tommaso.sacchetti@gmail.com>
2021-05-05 23:55:34 +0200

    Finalizing structure

--- simulation/start.sh
+++ simulation/start.sh
@@ -1,3 +1,7 @@
-#!/bin/zsh
+#!/bin/bash
 
-ping 1.1.1.1
+xfce4-terminal -e 'bash -c "./ap/ap; bash"' -T "AP"
+
+xfce4-terminal -e 'bash -c "./mitm/mitm; bash"' -T "MitM"
+
+xfce4-terminal -e 'bash -c "./client/client; bash"' -T "Client"
